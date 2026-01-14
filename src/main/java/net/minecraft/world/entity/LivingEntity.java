1567b6f99c5a62bab5f7f16b994846f7cf0a40a4
unknownbbqrr <unknownbbqr@gmail.com>
2026-01-14 04:30:39 +0300

    LivingEntity placeholder eklendi

---
+++ src/main/java/net/minecraft/world/entity/LivingEntity.java
@@ -0,0 +1,6 @@
+package net.minecraft.world.entity;
+
+// Placeholder for LivingEntity Fire Totem hook
+public abstract class LivingEntity {
+    // TODO: checkTotemDeathProtection Fire Totem logic buraya eklenecek
+}
